99bfb58c5cf713aca684b766d0525136c11a0d1c
himanshnigam <himanshnigam1202@gmail.com>
2024-12-10 10:55:37 +0530

    Remove debugger and pagy metadata

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,24 @@ class PostsController < ApplicationController
   def index
     @q = Post.ransack(params[:q])
     @pagy, @posts = pagy(@q.result(distinct: true).order(created_at: :desc))
-
+    
+    simplified_pagination = {
+      scaffold_url: pagy_metadata(@pagy)[:scaffold_url],
+      first_url: pagy_metadata(@pagy)[:first_url],
+      prev_url: pagy_metadata(@pagy)[:prev_url],
+      page_url: pagy_metadata(@pagy)[:page_url],
+      next_url: pagy_metadata(@pagy)[:next_url],
+      last_url: pagy_metadata(@pagy)[:last_url],
+      count: pagy_metadata(@pagy)[:count],
+      page: pagy_metadata(@pagy)[:page],
+      limit: pagy_metadata(@pagy)[:limit]
+    }
+  
     render json: {
-      pagination: pagy_metadata(@pagy),
+      pagination: simplified_pagination,
       posts: ActiveModel::Serializer::CollectionSerializer.new(@posts, each_serializer: PostSerializer),
       current_posts_count: @posts.size
     }, status: :ok
-    debugger
   end
 
   def show
